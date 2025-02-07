8b827b82753fbecc5c38665a5698d5f3732a3e3a
Ali_Ahmed <aly88664422@gmail.com>
2025-02-07 20:26:03 +0200

    api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetchRandomUserData = async function (){
     try{
-        const response = await fetch('https://api.nasa.gov:443');
+        const response = await fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY');
         if (!response.ok) {
             throw new Error('Failed to fetch data');
         }
