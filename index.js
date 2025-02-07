081da8648feabdcb3e4194b1b06d23379987c59d
Ali_Ahmed <aly88664422@gmail.com>
2025-02-07 20:37:25 +0200

    fixing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fetchRandomUserData = async function (){
+const fetchAsteroid = async function (){
     try{
         const response = await fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY');
         if (!response.ok) {
